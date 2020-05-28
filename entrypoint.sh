627fa94ebaaa8cad4c931477e7583e7d45fc441d
ARFAOUI Marouene <arfaoui.marouene.devops@gmail.com>
2020-05-28 16:58:27 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-( echo yes; echo $ANYCONNECT_PASSWORD ) | openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --timestamp
+( echo yes; echo $ANYCONNECT_PASSWORD ) | openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --usergroup=Sofrecom-STN-VPN --no-cert-check --timestamp 
